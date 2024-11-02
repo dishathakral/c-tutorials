cf189aafa6652754517735105fd7726db152d9d9
Disha Thakral <disha@Dishas-MacBook-Air.local>
2024-11-02 14:06:01 +0530

    Your commit message

--- l1.cpp
+++ l1.cpp
@@ -1,4 +1,8 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
 using namespace std;
 struct node{
 int data;
---
+++ my_std.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <deque>
+// Add other headers as needed
